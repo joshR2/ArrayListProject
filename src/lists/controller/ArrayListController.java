da71a44a17872adccb1b9e03c1f1fb663758f732
joshR2 <josh2cool123@gmail.com>
2015-10-07 09:57:54 -0600

    Array list update
    Wrote code on array lists

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,11 +1,29 @@
 package lists.controller;
 
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+			firstWords = new ArrayList<String>();
+	}
 	
 	public void start()
 	{
-		
-		
+		firstWords.add("ahladlfhhjalfd");
 	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) + "is at spot#" + spot);
+		}
+			
+	}
+	
 }
